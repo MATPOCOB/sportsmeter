950e2fa458dccf2915cbe825228f1b2898eb7994
Kirill Klenski <kirill.klenski@gmail.com>
2013-06-12 01:24:24 +0300

    output sports, use lambdas ;-)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,26 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
 
-import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import models.*;
+import static java.util.Arrays.asList;
 
 public class Application extends Controller {
 
+  enum SportType {
+    RUNNING,
+    ORIENTIERING,
+    BIKE,
+    MOUNTAIN_BIKE,
+    SWIMMING
+  }
+
   public static void index() {
-    render();
+    List<SportType> sports = asList(SportType.values()).stream().filter((it) -> (it.ordinal() % 2 == 1)).collect(Collectors.<SportType>toList());
+    render(sports);
   }
 
 }
