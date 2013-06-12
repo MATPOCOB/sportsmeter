518785b24613d67cbc6043822923b49208a836a5
Kirill Klenski <kirill.klenski@gmail.com>
2013-06-12 09:49:48 +0300

    lamdas removed, not compatible with play 1.2.5

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,24 +2,18 @@ package controllers;
 
 import play.mvc.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.Arrays.asList;
-
 public class Application extends Controller {
 
   enum SportType {
     RUNNING,
-    ORIENTIERING,
+    ORIENTEERING,
     BIKE,
     MOUNTAIN_BIKE,
     SWIMMING
   }
 
   public static void index() {
-    List<SportType> sports = asList(SportType.values()).stream().filter((it) -> (it.ordinal() % 2 == 1)).collect(Collectors.<SportType>toList());
+    SportType[] sports = SportType.values();
     render(sports);
   }
 
