59753bf07a41cba13322e2c76e3b3be27fe9bdad
Kirill Klenski <kirill.klenski@gmail.com>
2013-06-12 10:54:10 +0300

    correctly dispaying sport types

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,18 +2,23 @@ package controllers;
 
 import play.mvc.*;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class Application extends Controller {
 
   enum SportType {
     RUNNING,
     ORIENTEERING,
-    BIKE,
+    WALKING,
     MOUNTAIN_BIKE,
-    SWIMMING
+    SWIMMING,
+    ROLLER_BLADES
   }
 
   public static void index() {
-    SportType[] sports = SportType.values();
+    List<SportType> sports = asList(SportType.values());
     render(sports);
   }
 
