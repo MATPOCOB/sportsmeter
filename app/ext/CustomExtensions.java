aa7cb81780d1557cc3499d7db2aecba4abc20d2c
Kirill Klenski <kirill.klenski@gmail.com>
2013-06-29 11:17:52 +0300

    uses UTC timezone to format duration

--- app/ext/CustomExtensions.java
+++ app/ext/CustomExtensions.java
@@ -6,14 +6,22 @@ import play.templates.JavaExtensions;
 import java.lang.String;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class CustomExtensions extends JavaExtensions {
 
   public static String formatTime(Date date) {
-    return new SimpleDateFormat(Config.DATE_FORMAT.split(" ")[0]).format(date);
+    return new SimpleDateFormat(Config.TIME_FORMAT).format(date);
   }
 
   public static String formatDate(Date date) {
-    return new SimpleDateFormat(Config.DATE_FORMAT.split(" ")[1]).format(date);
+    return new SimpleDateFormat(Config.DATE_FORMAT).format(date);
+  }
+
+  public static String formatTimeUTC(Date date) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat(Config.TIME_FORMAT);
+    dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    return dateFormat.format(date);
   }
 }
--- app/models/Config.java
+++ app/models/Config.java
@@ -3,6 +3,16 @@ package models;
 import play.Play;
 
 public class Config {
-  public static final String DATE_FORMAT = Play.configuration.getProperty("date.format");
-  public static final String TIME_FORMAT = Play.configuration.getProperty("time.format", "HH:mm:ss");
+  public static String DATETIME_FORMAT = Play.configuration.getProperty("date.format");
+  public static String DATE_FORMAT = dateFormat(DATETIME_FORMAT);
+  public static String TIME_FORMAT = timeFormat(DATETIME_FORMAT);
+
+  private static String dateFormat(String datetime) {
+    return datetime.split(" ")[1];
+  }
+
+  private static String timeFormat(String datetime) {
+    return datetime.split(" ")[0];
+  }
 }
+
